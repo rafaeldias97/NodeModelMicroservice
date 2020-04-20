9d4f3575a9872cdf092ff3f052345ea7f24a88d2
Rafael Dias <rafael.cdc97@gmail.com>
2020-04-20 18:10:22 -0300

    Alteraçao Model BFFGateway

--- BFFGateway/src/routes/CustommerController.js
+++ BFFGateway/src/routes/CustommerController.js
@@ -1,16 +1,18 @@
 const typeDefs = `
   type Custommer {
-    id: ID
-    status: String
-    fullName: String!
-    accountId: String
+    id: ID,
+    status: String,
+    fullName: String,
+    accountId: String,
+    userLogin: String,
+    userPassword: String
   }
   type Query {
     getCustommers: [Custommer]
     getCustommer(id: ID!): Custommer
   }
   type Mutation {
-    addCustommer(fullName: String!): Custommer
+    addCustommer(fullName: String!, userLogin: String!, userPassword: String!): Custommer
     updateCustommer(fullName: String!): Custommer
     deleteCustommer(id: ID!): Custommer
   }
